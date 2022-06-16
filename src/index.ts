102fceef4408049bc3093985e17200431d8bacd9
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2022-06-16 23:04:43 +0200

    adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+app.use(cors())
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
